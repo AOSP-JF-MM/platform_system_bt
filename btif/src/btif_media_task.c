4f4c29b5c01181891dac55ff76619511c623b732
Ayan Ghosh <abghosh@codeaurora.org>
2016-03-03 18:34:48 +0530

    Increase number of semaphores for media workqueue
    As mediatask allows only 128 pending workqueue enqueue semaphores
    to be created hence on Suspend confirm delay alarm callbacks run
    out of that semaphore count and 129th instance of it gets blocked
    and corresponding alarm_callback gets stuck.
    Now later on receiving suspend ack, mediatask goes for freeing the
    media alarm which eventually waits for pending callback to come out.
    But as that never happens as callback is stuck so eventually mediatask
    also gets blocked and SUSPEND ACK to Audio HAL also gets blocked.
    So this phenomenon eventually leads to deadlock and later whosoever
    tries to make use of audio therad runs into ANR.
    
    This fix ensures that the current proposed value of semaphore/ workqueue
    element cound is made so big that is capable of handling delay of 15s
    (Max delay entertained for any AV signalling commands) for
    suspend confirm case.
    
    Change-Id: I917e2e0651cfc93837c483cd6f4f94d8aa933fa8

--- btif/src/btif_media_task.c
+++ btif/src/btif_media_task.c
@@ -244,6 +244,7 @@ enum {
 #define PACKET_PLAYED_PER_TICK_44 7
 #define PACKET_PLAYED_PER_TICK_32 5
 #define PACKET_PLAYED_PER_TICK_16 3
+#define MAX_MEDIA_WORKQUEUE_SEM_COUNT 1024
 
 typedef struct
 {
@@ -855,7 +856,7 @@ bool btif_a2dp_start_media_task(void)
     btif_media_cmd_msg_queue = fixed_queue_new(SIZE_MAX);
 
     /* start a2dp media task */
-    worker_thread = thread_new("media_worker");
+    worker_thread = thread_new_sized("media_worker", MAX_MEDIA_WORKQUEUE_SEM_COUNT);
     if (worker_thread == NULL)
         goto error_exit;
 
